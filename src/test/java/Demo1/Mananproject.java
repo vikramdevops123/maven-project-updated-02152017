7c17e787baae373f462d016a3bb45c8e59f84df6
vikramdevops123 <33900807+vikramdevops123@users.noreply.github.com>
2018-01-14 00:00:56 +0530

    Update Mananproject.java

--- src/test/java/Demo1/Mananproject.java
+++ src/test/java/Demo1/Mananproject.java
@@ -1,11 +1,18 @@
 package Demo1;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Mananproject {
@@ -13,7 +20,7 @@ public class Mananproject {
 	String URL="http://mycutebaby.in/contest/participant/?n=5a54a5653b746&utm_source=wsapp_share&utm_campaign=January_2018&utm_medium=shared&utm_term=wsapp_shared_5a54a5653b746&utm_content=participant";
 	
 	@Test
-	public void mananpage()
+	public void mananpage() throws Exception
 	{
 		
 		WebDriver driver=new FirefoxDriver();
@@ -29,6 +36,10 @@ public class Mananproject {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div[9]/div[1]/form/a/span[2]")));
 		
 		driver.findElement(By.xpath("/html/body/div[1]/div/div[9]/div[1]/form/a/span[2]")).click();
+		
+		File src= ((TakesScreenshot)driver). getScreenshotAs(OutputType. FILE);
+		
+		FileUtils. copyFile(src, new File("D:/Printscreens/"+System. currentTimeMillis()+".png"));
 		
 		driver.close();
 	}
